ebf298f7b36a108481dd625dc28c93e42d9100d4
TheBrue1 <113998134+TheBrue1@users.noreply.github.com>
2022-12-02 10:35:08 -0300

    attt

--- JavaScript/scrollReveal.js
+++ JavaScript/scrollReveal.js
@@ -1,7 +1,7 @@
 window.sr = ScrollReveal({ reset: true })
 
 ScrollReveal().reveal('.area-2', {
-    delay: 800,
+    delay: 500,
     rotate:{
         x:100,
         y:0,
@@ -9,7 +9,7 @@ ScrollReveal().reveal('.area-2', {
     }
 })
 ScrollReveal().reveal('.area-3',{
-    delay:800,
+    delay:500,
     rotate:{
         x:100,
         y:0,
@@ -17,7 +17,7 @@ ScrollReveal().reveal('.area-3',{
     }
 })
 ScrollReveal().reveal('.area-4',{
-    delay:800,
+    delay:500,
     rotate:{
         x:100,
         y:0,
@@ -25,17 +25,9 @@ ScrollReveal().reveal('.area-4',{
     }
 })
 ScrollReveal().reveal('.area-5',{
-    delay:800,
-    rotate:{
-        x:100,
-        y:0,
-        z:0
-    }
-})
-ScrollReveal().reveal('.bounce-in-bck',{
     delay:1000,
     rotate:{
-        x:0,
+        x:100,
         y:0,
         z:0
     }
